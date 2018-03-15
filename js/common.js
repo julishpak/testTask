5bf810cbe71f34c17ca4154948a1d22ea5a15b72
shpakweb <shpakweb@gmail.com>
2018-03-16 00:28:07 +0200

    fixed markup and styles

--- js/common.js
+++ js/common.js
@@ -42,10 +42,17 @@ $('.scrollup').click(function(){
 
 $(document).ready(function() {
 
-	$(".main_mnu_buttons").click(function(){
-		$(".main_mnu ul").slideToggle();
-
-	}	)
+	$(".main-mnu-button").click(function(){
+		$(".main-mnu-button").toggleClass("opened");
+		if($(".main-mnu ul").hasClass("opened")){
+			$(".main-mnu ul").removeClass('fadeInRight').addClass('fadeOutRight');
+		}
+		else{
+			$(".main-mnu ul").removeClass('fadeOutRight').addClass('animated fadeInRight');
+		}
+		$(".main-mnu ul").toggleClass("opened")
+	
+	})
 	/*change logotip */
 	
 	
@@ -95,7 +102,7 @@ $(document).ready(function() {
 	//Документация: http://fancybox.net/howto
 	//<a class="fancybox"><img src="image.jpg" /></a>
 	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
-	$(".fancybox").fancybox();
+	//$(".fancybox").fancybox();
 
 	//Навигация по Landing Page
 	//$(".top_mnu") - это верхняя панель со ссылками.
@@ -104,18 +111,18 @@ $(document).ready(function() {
 
 	//Добавляет классы дочерним блокам .block для анимации
 	//Документация: http://imakewebthings.com/jquery-waypoints/
-	$(".block").waypoint(function(direction) {
-		if (direction === "down") {
-			$(".class").addClass("active");
-		} else if (direction === "up") {
-			$(".class").removeClass("deactive");
-		};
-	}, {offset: 100});
+	// $(".block").waypoint(function(direction) {
+	// 	if (direction === "down") {
+	// 		$(".class").addClass("active");
+	// 	} else if (direction === "up") {
+	// 		$(".class").removeClass("deactive");
+	// 	};
+	// }, {offset: 100});
 
 	//Плавный скролл до блока .div по клику на .scroll
 	//Документация: https://github.com/flesler/jquery.scrollTo
-	$("a.scroll").click(function() {
-		$.scrollTo($(".div"), 800, {
+	$(".scroll").click(function() {
+		$.scrollTo($("#About_us"), 800, {
 			offset: -90
 		});
 	});
